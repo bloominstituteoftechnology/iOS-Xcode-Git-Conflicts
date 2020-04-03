d88e252eeae5d9620b295adb9be4d53074d664c5
Paul Solt <PaulSolt@gmail.com>
2020-04-03 12:30:21 -0400

    Added login starting point

---
+++ PasswordScreenAutoLayout/ContentTableView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentTableView.swift
+//  PasswordScreenAutoLayout
+//
+//  Created by Paul Solt on 4/3/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ContentTableView: UITableViewController {
+    
+    lazy var mockData: [Int] = {
+        var array = [Int]()
+        for i in 0...100 {
+            array.append(i)
+        }
+        return array
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let element = mockData[indexPath.row]
+        cell.textLabel?.text = "Fake content row: \(element)"
+        return cell
+    }
+    
+}
